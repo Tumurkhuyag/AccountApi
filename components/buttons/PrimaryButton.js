0070754a064647b8386191ade31a7e5acfb5c6e4
Tumurkhuyag <tomor.enkh@gmail.com>
2019-12-05 17:58:52 +0800

    Added icon to the PrimaryButton component

--- components/buttons/PrimaryButton.js
+++ components/buttons/PrimaryButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
-import { RobotoText } from '../StyledText'
+import { RobotoText } from '../StyledText';
+import FontIcon from '../icons/FontIcon';
 
 export function PrimaryButton(props) {
     return(
-        <TouchableOpacity style={{height: 40, backgroundColor: '#000', justifyContent: 'center', alignItems: 'center',}} >
+        <TouchableOpacity style={{flexDirection: 'row', height: 40, backgroundColor: '#000', justifyContent: 'center', alignItems: 'center',}} >
+            <FontIcon font={props.iconType} size={props.iconSize} color={props.iconColor} fontIcon={props.fontIcon} style={{marginRight: 10,}}/>
             <RobotoText font="regular" style={{color: '#fff', fontSize: 17}}>{props.text}</RobotoText>
         </TouchableOpacity>
     );
