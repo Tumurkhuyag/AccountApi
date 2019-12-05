4fae38cc48e88a10d3e44249250ff4ddcd6a6224
Tumurkhuyag <tomor.enkh@gmail.com>
2019-12-05 17:59:09 +0800

    Created SecondaryButton component

---
+++ components/buttons/SecondaryButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import { RobotoText } from '../StyledText'
+import FontIcon from '../icons/FontIcon';
+
+export function SecondaryButton(props) {
+    return(
+        <TouchableOpacity style={{flexDirection: 'row', height: 40, borderColor: '#E6E6E6', borderWidth: 1, justifyContent: 'center', alignItems: 'center',}} >
+            <FontIcon font={props.iconType} size={props.iconSize} color={props.iconColor} fontIcon={props.fontIcon} style={{marginRight: 10,}}/>
+            <RobotoText font="regular" style={{color: '#000', fontSize: 17}}>{props.text}</RobotoText>
+        </TouchableOpacity>
+    );
+}
